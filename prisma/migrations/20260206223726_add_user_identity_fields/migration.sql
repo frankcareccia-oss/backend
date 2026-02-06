db139cff768720aa573cd75e4367f4c65f07280f
Frank Careccia <frank.careccia@gmail.com>
2026-02-06 14:39:09 -0800

    chore(schema): add user identity fields

---
+++ prisma/migrations/20260206223726_add_user_identity_fields/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firstName" TEXT,
+ADD COLUMN     "lastName" TEXT,
+ADD COLUMN     "phoneCountry" TEXT NOT NULL DEFAULT 'US',
+ADD COLUMN     "phoneE164" VARCHAR(20),
+ADD COLUMN     "phoneRaw" TEXT;
+
+-- CreateIndex
+CREATE INDEX "User_phoneE164_idx" ON "User"("phoneE164");
